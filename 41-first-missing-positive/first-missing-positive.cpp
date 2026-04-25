c6871c333b6b28d9c7b6c52e35ae88c39422b85c
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-04-25 19:19:44 +0530

    Time: 4 ms (40.88%) | Memory: 55.1 MB (60.95%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -1,25 +1,41 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int i = 0;
-        int k =1;
-        while(i<nums.size())
-        {
-            if(nums[i] < k)
-            {
-                i++;
-            }
-            else if(nums[i] == k)
-            {
-                k++;
-                i++;
-            }
-            else
-            {
-                return k;
+        // sort(nums.begin(),nums.end());
+        // int i = 0;
+        // int k =1;
+        // while(i<nums.size())
+        // {
+        //     if(nums[i] < k)
+        //     {
+        //         i++;
+        //     }
+        //     else if(nums[i] == k)
+        //     {
+        //         k++;
+        //         i++;
+        //     }
+        //     else
+        //     {
+        //         return k;
+        //     }
+        // }
+        // return k;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            while (nums[i] >= 1 && nums[i] <= n && 
+                   nums[nums[i] - 1] != nums[i]) {
+                swap(nums[i], nums[nums[i] - 1]);
             }
         }
-        return k;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        return n + 1;
     }
 };
