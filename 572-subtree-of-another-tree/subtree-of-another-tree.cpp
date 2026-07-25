105857975f62b90dbe20c069bb62bf7ff6bbcefe
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-07-25 23:34:52 +0530

    Time: 1 ms (38.29%) | Memory: 29 MB (70.86%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(TreeNode* root1, TreeNode* root2)
+    {
+        if(root1 == NULL && root2 == NULL)
+        {
+            return true;
+        }
+        if(root1 != NULL && root2 != NULL && root1->val == root2->val)
+        {
+            return check(root1->left,root2->left) && check(root1->right,root2->right);
+        }
+        return false;
+    }
+    bool ans(TreeNode* root, TreeNode* subRoot)
+    {
+        if(root == NULL && subRoot == NULL)
+        {
+            return true;
+        }
+        else if(root == NULL && subRoot != NULL)
+        {
+            return false;
+        }
+        else if(root != NULL && root->val == subRoot->val && check(root,subRoot))
+        {
+            return true;
+        }
+        else
+        {
+            return ans(root->left,subRoot) || ans(root->right,subRoot);
+        }
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        return ans(root,subRoot);
+    }
+};
