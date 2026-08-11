c64faf471482e4e44df7d7b3bd60f4ac6eadc94a
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-08-11 17:19:40 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (70.00%) - LeetSync

---
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> dp;
+    int tribonacci(int n) {
+        if(n == 0)
+        {
+            return 0;
+        }
+        if(n == 1 || n == 2)
+        {
+            return 1;
+        }
+        int first = 0;
+        int second = 1;
+        int third = 1;
+        for(int i = 3;i<=n;i++)
+        {
+            int curr = first + second + third;
+            first = second;
+            second = third;
+            third = curr;
+        }
+        return third;
+        
+    }
+};
